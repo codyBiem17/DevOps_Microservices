cb0bd8e795defbc0a6c2ffaa74b8e59313d743c4
EC2 Default User <ec2-user@ip-172-31-91-70.ec2.internal>
2022-08-24 11:12:10 +0000

    project 4 is done

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,20 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="maryambee/microservice-project:v1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run microservice-project \
+    --image=maryambee/microservice-project:v1.0 \
+    --port=80 \
+    --labels app=microservice-project
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward microservice-project 8000:80
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="maryambee/microservice-project:v1.0"
+
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag microservice-project $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
